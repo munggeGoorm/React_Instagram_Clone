102f3d3bf128302a2e773c761a404d74b95dad3e
seungyeah <closer.fz@gmail.com>
2023-08-04 19:21:26 +0900

    modyfiy PostsPage

--- src/pages/MainPage/index.js
+++ src/pages/MainPage/index.js
@@ -1,6 +1,7 @@
 import logo from '../../logo.svg';
 import PostItem from '../../components/PostItem/index';
 import React, {useEffect, useState} from 'react'
+import PostsPage from '../PostsPage/index';
 import '../../App.css';
 import axios from "axios";
 import UnLoginedPage from '../AccountPage/index';
@@ -20,31 +21,7 @@ export default function Index() {
                 !isAuthenticated ?
                     <UnLoginedPage setIsAuthenticated={setIsAuthenticated}/>
                     :
-                    <div className="w-full h-full m-auto  flex justify-center ">
-                        {/*navigation*/}
-                        <div className="w-[250px] 2xl:w-[300px]  h-screen fixed left-0 top-0
-                        flex-col justify-center items-start space-y-4 p-4 bg-blue-50  ">
-                            <h1 className="text-4xl text-center font-bold">Instagram</h1>
-                            <a href="/" className="text-2xl text-start font-bold block">Home</a>
-                            <a href="/" className="text-2xl text-start font-bold block">Add Post</a>
-                            <a href="/" className="text-2xl text-start font-bold block">Profile</a>
-                        </div>
-
-                        {/*posts*/}
-                        <div className=" h-full w-2/5  flex-col  bg-amber-50">
-                            <PostItem/>
-                            <PostItem/>
-                            <PostItem/>
-                        </div>
-
-                        {/*login*/}
-                        <div className="fixed top-0 right-0">
-                            <div>
-                                로그인됨
-                            </div>
-                            <button onClick={() => setIsAuthenticated(false)}>로그아웃</button>
-                        </div>
-                    </div>
+                    <PostsPage/>
             }
         </div>
     )
