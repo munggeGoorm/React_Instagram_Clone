304f09e140b676ffabecf03c5cce6daab8ff7034
ydmins <dr.ydmins@gmail.com>
2023-08-10 14:37:30 +0900

    Edit 로그인 기능 수정 및 로그아웃 버튼 구현

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 export default axios.create({
     baseURL: "http://localhost:8080/api/v1",
+    // baseURL: "49.142.12.7/api/v1",
     headers: {
         "Content-type":"application/json;charset=UTF-8",
         "Accept": "application/json",
