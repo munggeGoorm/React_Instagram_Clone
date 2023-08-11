fd0d50b2588bdc90f94bf50d0cde80ba45a858ef
yodafriend <83869868+yodafriend@users.noreply.github.com>
2023-08-11 19:01:26 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Routes, Route} from 'react-router-dom';
 import MainPage from "./pages/MainPage/index.js";
 import DirectInboxPage from "./pages/DirectInboxPage/index.js";
 import ProfilePage from "./pages/ProfilePage/Profile.js";
+import EditProfile from './pages/ProfilePage/EditProfile.js';
 import EmailSignupPage from './pages/AccountPage/EmailSignup.js';
 import LoginPage from './pages/AccountPage/Login.js';
 
@@ -14,6 +15,7 @@ export default function App() {
             <Route path="accounts/emailsignup" element={<EmailSignupPage/>}/>
             <Route path="accounts/login" element={<LoginPage/>}/>
             <Route path="profile" element={<ProfilePage/>}/>
+            <Route path="/profile/EditProfile" element={<EditProfile/>}/>
             <Route path="directinbox" element={<DirectInboxPage/>}/>
           </Routes>
       </div>
